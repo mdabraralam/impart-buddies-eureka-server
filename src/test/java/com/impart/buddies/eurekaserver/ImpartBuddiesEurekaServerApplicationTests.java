e20633c63339cccc9dd7a877efe60a7b17da1bae
mdabraralam <30721416+mdabraralam@users.noreply.github.com>
2025-03-19 01:11:52 +0530

    initail development

---
+++ src/test/java/com/impart/buddies/eurekaserver/ImpartBuddiesEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impart.buddies.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpartBuddiesEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
